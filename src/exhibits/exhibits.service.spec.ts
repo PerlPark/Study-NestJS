12f41d04978957f20d19a8cc0b368b770c82d1b8
perlpark <hello@jinjoo.me>
2023-01-19 22:43:44 +0900

    feat: 테스트 케이스 추가

--- src/exhibits/exhibits.service.spec.ts
+++ src/exhibits/exhibits.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExhibitsService } from './exhibits.service';
 
@@ -14,5 +15,27 @@ describe('ExhibitsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a exhibition', () => {
+      const exhibition = service.getOne(1);
+      expect(exhibition).toBeDefined();
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Not Found Exhibit width ID: 999');
+      }
+    });
   });
 });
