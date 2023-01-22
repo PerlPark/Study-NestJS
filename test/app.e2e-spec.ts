dd0b945c44f33fb2e0f4544fd41518d79c8bfd4e
perlpark <hello@jinjoo.me>
2023-01-22 11:51:18 +0900

    feat: e2e 테스트 코드 작성

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,85 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import dummy from '../src/dummy';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/health (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect('health');
+  });
+
+  describe('/exhibits', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/exhibits')
+        .expect(200)
+        .expect(dummy);
+    });
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/exhibits')
+        .send({
+          title: 'Test',
+          startDate: 2023,
+          endDate: 2024,
+          location: ['서울', '예술의 전당'],
+        })
+        .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/exhibits')
+        .send({
+          title: 'Test',
+          startDate: 2023,
+          endDate: 2024,
+          location: ['서울', '예술의 전당'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/exhibits').expect(404);
+    });
+  });
+
+  describe('/exhibits/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/exhibits/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/exhibits/999').expect(404);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/exhibits/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/exhibits/1').expect(200);
+    });
   });
 });
